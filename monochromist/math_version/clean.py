d9845eff7af45647a4fb639302cbc01428363f57
Bogdan Neterebskii <neterebskii@anylogic.com>
2021-10-14 09:41:30 +0300

    add independent run function

--- monochromist/math_version/clean.py
+++ monochromist/math_version/clean.py
@@ -19,7 +19,6 @@ def clean_image(img: Image, settings: Settings) -> Image:
 
     threshold = find_threshold(values, settings)
     cleaned_array = values < threshold
-    print(np.sum(cleaned_array.flatten()))
     colored_image = color_image(img, cleaned_array, settings)
 
     # TODO: remove artifacts with alone pixels
